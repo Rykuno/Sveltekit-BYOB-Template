c25129cbe545f4c819083e422234fb74d223dab3
rykuno <rykuno@gmail.com>
2024-03-19 09:22:17 -0500

    excluded oslo from vite. Added remaining http methods w/ fallback

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,5 @@
 export const load = async ({ locals }) => {
 	const authedUser = await locals.getAuthedUser();
 
-	console.log('AUTHED USER => ', authedUser);
-
-	// console.log('AUTHED USER => ', data, error, status);
-	// const { data, error, status } = await parseApiResponse(locals.api.users.$get());
-	// if (error) {
-	// 	console.log('ERROR => ', error, status);
-	// 	return { users: null };
-	// }
-
 	return { users: [] };
 };
--- src/routes/api/[...slugs]/+server.ts
+++ src/routes/api/[...slugs]/+server.ts
@@ -5,3 +5,4 @@ export const GET: RequestHandler = ({ request }) => app.fetch(request);
 export const PUT: RequestHandler = ({ request }) => app.fetch(request);
 export const DELETE: RequestHandler = ({ request }) => app.fetch(request);
 export const POST: RequestHandler = ({ request }) => app.fetch(request);
+export const PATCH: RequestHandler = ({ request }) => app.fetch(request);
